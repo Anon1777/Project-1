f8a6a40d5a0811b142d7dcdd7592da0dd1917e8e
Anon1777 <farrelj5@tcnj.edu>
2024-08-27 14:40:02 -0400

    Welcome to Java

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+// Welcome.java
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java!"); // prints "Welcome to Java!" and then a new line
+    } // closes main
+} // closes Welcome
